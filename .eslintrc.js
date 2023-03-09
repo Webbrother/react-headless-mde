3f528d0e907f337a7a979a4ae778fc6808c897df
webbrother <komarov.vitaliy@gmail.com>
2023-03-09 22:47:40 +0400

    "parserOptions.project" property for @typescript-eslint/parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: ['./tsconfig.json'], // Specify it only for TypeScript files
   },
   plugins: ['react'],
   rules: {},

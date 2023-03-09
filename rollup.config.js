587c71b2cf8ae45208417a6f770453d7496ab987
webbrother <komarov.vitaliy@gmail.com>
2023-03-09 23:22:13 +0400

    include config files to tsconfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,31 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
-        sourcemap: true
+        format: 'cjs',
+        sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: "esm",
-        sourcemap: true
-      }
+        format: 'esm',
+        sourcemap: true,
+      },
     ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" })
-    ],
-    external: ["react", "react-dom"]
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
+    external: ['react', 'react-dom'],
   },
   {
-    input: "dist/esm/src/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()]
-  }
+    input: 'dist/esm/src/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
 ];

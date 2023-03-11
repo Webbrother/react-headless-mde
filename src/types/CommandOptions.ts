0bab24f31e08e27c50887f9095ae3fd205e3292e
webbrother <komarov.vitaliy@gmail.com>
2023-03-11 23:53:25 +0400

    method-signature-style eslnt error fix

--- src/types/CommandOptions.ts
+++ src/types/CommandOptions.ts
@@ -16,12 +16,12 @@ export interface TextController {
   // Replaces the current selection with the new text. This will make the new selectedText to be empty, the
   // selection start and selection end will be the same and will both point to the end
   // @param text Text that should replace the current selection
-  replaceSelection(text: string): TextState;
+  replaceSelection: (text: string) => TextState;
 
   // Selects the specified text range
   // @param selection
-  setSelectionRange(selection: SelectionRange): TextState;
+  setSelectionRange: (selection: SelectionRange) => TextState;
 
   // Get the current text state
-  getState(): TextState;
+  getState: () => TextState;
 }

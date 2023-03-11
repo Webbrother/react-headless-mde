53a15a75c19b8f0719648ece4235b67f558e9c42
webbrother <komarov.vitaliy@gmail.com>
2023-03-11 23:54:50 +0400

    cleanup

--- src/types/CommandOptions.ts
+++ src/types/CommandOptions.ts
@@ -15,7 +15,7 @@ export interface TextState {
 export interface TextController {
   // Replaces the current selection with the new text. This will make the new selectedText to be empty, the
   // selection start and selection end will be the same and will both point to the end
-  // @param text Text that should replace the current selection
+  // @param text that should replace the current selection
   replaceSelection: (text: string) => TextState;
 
   // Selects the specified text range

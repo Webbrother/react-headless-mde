06f4f8bd3bdbcb378b7c67e369e00035a7250325
webbrother <komarov.vitaliy@gmail.com>
2023-03-13 18:23:17 +0400

    demo updated

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,46 +1,60 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Box, ChakraProvider, HStack, Textarea } from '@chakra-ui/react';
-import { faBold, faItalic, faCode, faHeading } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBold, faItalic, faCode, faHeading, faFile } from '@fortawesome/free-solid-svg-icons';
 import { attachment, bold, code, headingLevel1, italic, useTextAreaMarkdownEditor } from '../src';
 import { ToolbarButton } from './toolbar-button';
+import ReactMarkdown from 'react-markdown';
 
 const uploadMock = async () =>
   await new Promise<string>(resolve => {
     setTimeout(() => {
-      resolve('https://google.com');
-    }, 5000);
+      resolve('https://example.com');
+    }, 1000);
   });
 
 export const Demo = () => {
   const { ref } = useTextAreaMarkdownEditor();
+  const [isPreview, setIsPreview] = useState(false);
+  const [value, setValue] = useState('');
 
   return (
     <ChakraProvider>
       <Box p={3}>
         <HStack py={2}>
-          <ToolbarButton onClick={bold}>
-            <FontAwesomeIcon icon={faBold} />
-          </ToolbarButton>
-          <ToolbarButton onClick={italic}>
-            <FontAwesomeIcon icon={faItalic} />
-          </ToolbarButton>
-          <ToolbarButton onClick={code}>
-            <FontAwesomeIcon icon={faCode} />
-          </ToolbarButton>
-          <ToolbarButton onClick={headingLevel1}>
-            <FontAwesomeIcon icon={faHeading} />
-          </ToolbarButton>
+          <ToolbarButton onClick={bold} icon={faBold} />
+          <ToolbarButton onClick={italic} icon={faItalic} />
+          <ToolbarButton onClick={code} icon={faCode} />
+          <ToolbarButton onClick={headingLevel1} icon={faHeading} />
           <ToolbarButton
             onClick={() => {
-              attachment({ fileName: 'file-name', fileUrlPromise: uploadMock() });
+              attachment({ fileName: 'file-name.txt', fileUrlPromise: uploadMock() });
+            }}
+            icon={faFile}
+          />
+
+          <ToolbarButton
+            onClick={() => {
+              setIsPreview(val => !val);
             }}
           >
-            +
+            Preview
           </ToolbarButton>
         </HStack>
-        <Textarea ref={ref} placeholder="I'm a markdown editor" fontFamily={'monospace'} />
+
+        {isPreview ? (
+          <ReactMarkdown>{ref.current?.value ?? ''}</ReactMarkdown>
+        ) : (
+          <Textarea
+            ref={ref}
+            value={value}
+            onChange={e => {
+              setValue(e.target.value);
+            }}
+            placeholder="I'm a markdown editor"
+            fontFamily={'monospace'}
+          />
+        )}
       </Box>
     </ChakraProvider>
   );
